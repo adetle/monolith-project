a82365d6846858ed5779cde77f1387233b20957a
adetle <adetle022003@yahoo.com>
2022-10-03 11:48:00 +0100

    version 3 frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a014b6b254b5541f982c390f41f10403-282624722.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
